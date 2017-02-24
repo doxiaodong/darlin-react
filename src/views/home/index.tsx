981bfeac5003f8a19918aacb899a9a6222189fd5
duxiaodong <duxiaodong@darlin.me>
2017-02-24 12:46:58 +0800

    add async test

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,7 +2,21 @@ import * as React from 'react'
 
 export default class ViewIndex extends React.Component<{}, {}> {
 
+  async a() {
+    const x = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(10001)
+      }, 2000)
+    })
+    console.log(x)
+    return x
+  }
+
   render() {
+    const a = this.a()
+    a.then((d) => {
+      console.log(d)
+    })
     return (
       <div>nice react</div>
     )
