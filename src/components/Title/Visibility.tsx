ae37982037f6c7187a81da625d1e55b3eb0425fd
darlin <duxiaodong@darlin.me>
2017-03-05 13:12:52 +0800

    add Visibility

---
+++ src/components/Title/Visibility.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { observer } from 'mobx-react'
+import { LocalStorage } from 'base/local-storage'
+import i18nStore from 'stores/i18n'
+
+@observer
+export class Visibility extends React.Component<{ visibilityTitle: string }, {}> {
+
+  visibilityTitle: string = ''
+
+  visibilitychangeEvent = () => {
+    const state = document.visibilityState
+    if (state === 'hidden') {
+      LocalStorage.saveSession('visibilityChangeTitle', document.title)
+      document.title = this.visibilityTitle + i18nStore.t('common:titleSuffix')
+    }
+    if (state === 'visible') {
+      document.title = LocalStorage.getSession('visibilityChangeTitle')
+      LocalStorage.removeSession('visibilityChangeTitle')
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.visibilityTitle = nextProps.visibilityTitle
+  }
+
+  componentDidMount() {
+    this.visibilityTitle = this.props.visibilityTitle
+    document.addEventListener('visibilitychange', this.visibilitychangeEvent)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('visibilitychange', this.visibilitychangeEvent)
+  }
+
+  render() {
+    return (
+      null
+    )
+  }
+}
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
+import { observer } from 'mobx-react'
+import { Visibility } from './Visibility'
+import i18nStore from 'stores/i18n'
 
+@observer
 export class Title extends React.Component<{ title: string }, {}> {
 
   updateTitle(props) {
-    const nextTitle = props.title || ''
+    const nextTitle = (props.title || '') + i18nStore.t('common:titleSuffix')
     if (nextTitle !== document.title) {
       document.title = nextTitle
     }
@@ -18,8 +22,7 @@ export class Title extends React.Component<{ title: string }, {}> {
   }
 
   render() {
-    return (
-      null
-    )
+    const { t } = i18nStore
+    return <Visibility visibilityTitle={t('common:visibilityTitle')} />
   }
 }
