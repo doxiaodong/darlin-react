1c03c1814920809440c5be5f3a75aa59de2977a8
darlin <duxiaodong@darlin.me>
2017-03-05 22:30:25 +0800

    add device.js and sw

---
+++ src/sw.js
@@ -0,0 +1,49 @@
+const staticCacheName = 'static'
+const version = 'v5::'
+const cacheKeys = [
+  /static\.darlin\.me/,
+  /cdn\.tristana\.cc/,
+  /cdn\.mathjax\.org/,
+  /cdn\.jsdelivr\.net/
+]
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function(keys) {
+        return Promise.all(keys
+          .filter(function(key) {
+            return key.indexOf(version) !== 0
+          })
+          .map(function(key) {
+            return caches.delete(key)
+          })
+        )
+      })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  const request = event.request
+  const isCache = cacheKeys.some(k => k.test(request.url))
+  if (isCache) {
+    event.respondWith(
+      caches.match(request).then((response) => {
+        if (response) {
+          return response
+        }
+        return fetch(request.clone()).then((fetchResponse) => {
+
+          const responseClone = fetchResponse.clone()
+          caches.open(version + staticCacheName)
+            .then(cache => {
+              cache.put(request, responseClone)
+            })
+
+          return fetchResponse
+        })
+      })
+    )
+  }
+
+})
