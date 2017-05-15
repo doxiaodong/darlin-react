751d9cc9879eb89a13fc4237938134786b8a5758
duxiaodong <duxiaodong@darlin.me>
2017-05-15 13:07:30 +0800

    rename *.tsx -> *.ts

--- src/types/js-base64.d.ts
+++ src/types/js-base64.d.ts
@@ -1,15 +1,15 @@
-interface Base64Interface {
+interface IBase64 {
   VERSION: string
   encode(a: string): string
   encodeURI(a: string): string
   decode(a: string): string
 }
 
-interface JSBase64 {
-  Base64: Base64Interface
+interface IJSBase64 {
+  Base64: IBase64
 }
 
-declare const base64: JSBase64
+declare const base64: IJSBase64
 
 declare module 'js-base64' {
   export = base64
